fd864a5eab766d9655a6a6f675e55f69c4e460dd
Aidan <aidankmcalister@gmail.com>
2024-09-23 22:30:45 -0500

    click intercepted

--- scraper.py
+++ scraper.py
@@ -16,6 +16,16 @@ print("\nThis script will help you scrape LinkedIn profiles and automatically co
 print("Please put your LinkedIn credentials in the .env file\n")
 print("="*50)
 
+northeast_geourns = "%2C".join([
+    "104597301",  # Maine
+    "103506248",  # New Hampshire
+    "105677329",  # Vermont
+    "100506914",  # Massachusetts
+    "104150334",  # Rhode Island
+    "100448276",  # Connecticut
+    "105080838"   # New York
+])
+
 while True:
     try:
         amount = int(input("\nEnter the amount of people to connect to: "))
@@ -40,10 +50,13 @@ try:
 
     time.sleep(15)
 
-    driver.get(f'https://www.linkedin.com/search/results/people/?geoUrn=%5B%22103644278%22%5D&keywords={
-               search_query}&origin=FACETED_SEARCH&profileLanguage=%5B%22en%22%5D&sid=WJ)')
+    search_url = f'https://www.linkedin.com/search/results/people/?geoUrn=%5B{
+        northeast_geourns}%5D&keywords={search_query}&origin=FACETED_SEARCH&sid=!.@'
+
+    driver.get(search_url)
 
     people = []
+    current_page = 1
 
     while len(people) < amount:
         wait = WebDriverWait(driver, 10)
@@ -52,32 +65,79 @@ try:
 
         for person in peopleList:
             if len(people) < amount:
-                # driver.find_element(
-                #     By.CLASS_NAME, "search-reusables-modal__close-btn").click()
                 name_parent = person.find_element(
                     By.CLASS_NAME, "entity-result__title-text")
                 name = name_parent.find_element(
                     By.XPATH, ".//span[@dir='ltr']").find_element(
                     By.XPATH, ".//span[@aria-hidden='true']").text
-                people.append(name)
+                try:
+                    # Click the "Connect" button
+                    # connect_button = driver.find_element(
+                    #     (By.XPATH, ".//div[contains(@class, 'entity-result__actions')]//button[contains(text(), 'Connect')]"))
+                    connect_container = person.find_element(
+                        By.CLASS_NAME, "entity-result__actions")
+                    print("connect_container", connect_container)
+
+                    connect_button = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, "button.artdeco-button--secondary"))
+                    )
+                    connect_button.click()
+                    print("connect_button", connect_button)
+
+                    time.sleep(10)
+                    connect_button.click()
+
+                    # Wait for the popup to appear
+                    wait = WebDriverWait(driver, 10)
+                    add_note_button = wait.until(EC.element_to_be_clickable(
+                        (By.XPATH, "//button[@aria-label='Add a note']")))
+                    add_note_button.click()
+
+                    # Type the note
+                    note_textarea = wait.until(
+                        EC.presence_of_element_located((By.NAME, "message")))
+                    note_textarea.send_keys(
+                        f'''Hi {name},
+
+                        I hope you're doing well. My name is Aidan, and I'm a junior software engineer with some part-time and contract experience. I'm currently looking to transition into a full-time role and would love to hear your thoughts on navigating the job market, especially as someone without a traditional degree.
+
+                        If you have a few moments to chat, I'd really appreciate the chance to learn from your experience. Looking forward to hearing from you!
+
+                        Best,
+                        Aidan''')
+                    time.sleep(30)
+
+                    # Send the connection request
+                    # send_button = driver.find_element(
+                    #     By.XPATH, "//button[contains(@aria-label, 'Send invitation')]")
+                    # send_button.click()
+
+                    # Wait for the request to be sent
+                    time.sleep(2)
+                    status = "✅"
+                except Exception as e:
+                    status = "❌"
+                    print(f"Error: {e}")
+                people.append((name, status))
             else:
                 break
 
         if len(people) < amount:
             try:
-                wait = WebDriverWait(driver, 10)
-                next_button = wait.until(EC.element_to_be_clickable(
-                    (By.CSS_SELECTOR, "button.artdeco-pagination__button--next")))
-                next_button.click()
+                current_page += 1
+                next_page_url = f'https://www.linkedin.com/search/results/people/?geoUrn=%5B{
+                    northeast_geourns}%5D&keywords={search_query}&origin=FACETED_SEARCH&sid=!.@&page={current_page}'
+                driver.get(next_page_url)
+                time.sleep(3)
             except Exception as e:
                 print(
-                    f"An error occurred while trying to navigate to the next page: {e}")
+                    f"NEXT BUTTON ERROR: {e}")
                 break
     print("\n")
     print("Results:\n")
-    max_name_length = max(len(name) for name in people) + 4
-    for index, name in enumerate(people, start=1):
-        status = "SENT"
+    max_name_length = max(len(name) for name, _ in people) + 4
+    for index, (name, status) in enumerate(people, start=1):
         print(f"{index}. {name:<{max_name_length}} {status}")
     print("\n" + "="*50)
 
